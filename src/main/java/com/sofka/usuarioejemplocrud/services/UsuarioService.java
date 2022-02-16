f1393a6aff981d8cb2c8f531e802d5e676c4416f
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 15:56:13 -0500

    Implementación de los servicios de búsqueda por id, búsqueda por prioridad y eliminar usuario

--- src/main/java/com/sofka/usuarioejemplocrud/services/UsuarioService.java
+++ src/main/java/com/sofka/usuarioejemplocrud/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.sofka.usuarioejemplocrud.services;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import com.sofka.usuarioejemplocrud.models.UsuarioModel;
 import com.sofka.usuarioejemplocrud.repositories.UsuarioRepository;
@@ -8,6 +9,7 @@ import com.sofka.usuarioejemplocrud.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//Regla personal: En el servicio los métodos van en Inglés
 @Service
 public class UsuarioService {
     //Injección para usar aquí los métodos de la interfaz a la que se instancia
@@ -20,9 +22,28 @@ public class UsuarioService {
     }
 
     //Guardar usuarios
-    //Preguntar: Esto de aquí no tiene que ser un void?
     public UsuarioModel saveUsuario(UsuarioModel u){
         return uRepository.save(u);
+    }
+
+    //Obtener usuario por id
+    public Optional<UsuarioModel> getById(Long id){
+        return uRepository.findById(id);
+    }
+
+    //Obtener usuario(s) por prioridad
+    public ArrayList<UsuarioModel> getByPrioridad(Integer prioridad){
+        return uRepository.findByPrioridad(prioridad);
+    }
+
+    //Eliminar usuario por id
+    public boolean deleteUsuario(Long id){
+        try{
+            uRepository.deleteById(id);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
 
 
