3a6dea95463f3b669612710fe8be80a9ba2f32fc
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 15:55:10 -0500

    Implementación de método abstract p (porque es una interfaz) ra hacer buscar por un camÂpo en particularidadÂ

--- src/main/java/com/sofka/usuarioejemplocrud/repositories/UsuarioRepository.java
+++ src/main/java/com/sofka/usuarioejemplocrud/repositories/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.sofka.usuarioejemplocrud.repositories;
 
+import java.util.ArrayList;
+
 import com.sofka.usuarioejemplocrud.models.UsuarioModel;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,5 +10,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<UsuarioModel,Long>{
-    
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
+
+    //Otros métodos patrocinados por CrudRepository
+    //public abstract ArrayList<UsuarioModel> findByNombre(String nombre);
+    //public abstract ArrayList<UsuarioModel> findByEmail(String email);
 }
