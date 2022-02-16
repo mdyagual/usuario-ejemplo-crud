751f6a421eb5723f72836be6e34f0a5a3ba28e42
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 02:28:29 -0500

    Implementación de inyecciÂón de la interfaz a la que se instancia

--- src/main/java/com/sofka/usuarioejemplocrud/services/UsuarioService.java
+++ src/main/java/com/sofka/usuarioejemplocrud/services/UsuarioService.java
@@ -1,9 +1,30 @@
 package com.sofka.usuarioejemplocrud.services;
+import java.util.ArrayList;
+
+import com.sofka.usuarioejemplocrud.models.UsuarioModel;
+import com.sofka.usuarioejemplocrud.repositories.UsuarioRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-    
+    //Injección para usar aquí los métodos de la interfaz a la que se instancia
+    @Autowired
+    UsuarioRepository uRepository;
+
+    //Obtener todos los usuarios
+    public ArrayList<UsuarioModel> getUsuarios(){
+        return (ArrayList<UsuarioModel>) uRepository.findAll();
+    }
+
+    //Guardar usuarios
+    //Preguntar: Esto de aquí no tiene que ser un void?
+    public UsuarioModel saveUsuario(UsuarioModel u){
+        return uRepository.save(u);
+    }
+
 
 
     
