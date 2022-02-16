7cca57bc1c498e1f9b55c84acc3883a3c3d8d13f
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 02:29:04 -0500

    Implementación de inyecciÂón del servicio que se instancia

--- src/main/java/com/sofka/usuarioejemplocrud/controllers/UsuarioController.java
+++ src/main/java/com/sofka/usuarioejemplocrud/controllers/UsuarioController.java
@@ -1,9 +1,34 @@
 package com.sofka.usuarioejemplocrud.controllers;
 
 
+import java.util.ArrayList;
+
+import com.sofka.usuarioejemplocrud.models.UsuarioModel;
+import com.sofka.usuarioejemplocrud.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
-    
+    //Injección para usar aquí los métodos del servicio que se instancia
+    @Autowired
+    UsuarioService uService;
+
+    @GetMapping    
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return uService.getUsuarios();
+    }
+
+    @PostMapping
+    public UsuarioModel guardarUsuario(UsuarioModel u){
+        return this.uService.saveUsuario(u);
+    }
+
+
+
 }
