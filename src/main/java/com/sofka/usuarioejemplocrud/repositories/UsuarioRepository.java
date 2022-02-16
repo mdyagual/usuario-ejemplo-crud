4ebfc5be4905bb0f73b045aae8f4dea17e2fc72d
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 01:47:13 -0500

    Agregación de etiqueta para establecer rol de gestion de bases de datos con CrudRepository

--- src/main/java/com/sofka/usuarioejemplocrud/repositories/UsuarioRepository.java
+++ src/main/java/com/sofka/usuarioejemplocrud/repositories/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.sofka.usuarioejemplocrud.repositories;
 
 import org.springframework.stereotype.Repository;
 
+//Comentario para corregir
 @Repository
 public interface UsuarioRepository {
     
