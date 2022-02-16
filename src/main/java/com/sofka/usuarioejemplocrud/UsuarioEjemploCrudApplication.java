44ddc6524a02c68b1252b0cc94c1c301eda8ffd7
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-15 23:53:10 -0500

    Initial commit

---
+++ src/main/java/com/sofka/usuarioejemplocrud/UsuarioEjemploCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.usuarioejemplocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioEjemploCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioEjemploCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/usuarioejemplocrud/UsuarioEjemploCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.usuarioejemplocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioEjemploCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
