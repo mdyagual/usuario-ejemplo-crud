dc6b8bfdc5b632c024505f3254073537fec635c7
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-17 00:10:55 -0500

    Corrección de método abstract que no puede existir porque no existe el atributo en la clase UsuarioModel

--- src/main/java/com/sofka/usuarioejemplocrud/repositories/UsuarioRepository.java
+++ src/main/java/com/sofka/usuarioejemplocrud/repositories/UsuarioRepository.java
@@ -7,13 +7,10 @@ import com.sofka.usuarioejemplocrud.models.UsuarioModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/*Para mejorar
-1. Creación de método para buscar usuarios por una inicial
-*/
+
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<UsuarioModel,Long>{
     public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);    
-    public abstract ArrayList<UsuarioModel> findByInicialNombre(char c);
     
 }
