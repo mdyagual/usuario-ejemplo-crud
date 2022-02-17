b2f6ca7e212e1aca9e54ac209eb51335a40315b7
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 21:40:52 -0500

    Definición de mejoras: Implementación pendiente

--- src/main/java/com/sofka/usuarioejemplocrud/UsuarioEjemploCrudApplication.java
+++ src/main/java/com/sofka/usuarioejemplocrud/UsuarioEjemploCrudApplication.java
@@ -3,6 +3,10 @@ package com.sofka.usuarioejemplocrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*Para mejorar
+1. Crear Paquete 'exception' que permite crear excepciones personalizadas a usar en el CRUD
+*/
+
 @SpringBootApplication
 public class UsuarioEjemploCrudApplication {
 
