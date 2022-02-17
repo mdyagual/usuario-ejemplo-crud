d1109702f1401f61c26cc1c6a9c64ae2aadc2968
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-16 21:40:15 -0500

    Definición de mejoras: Implementación 1/3

--- src/main/java/com/sofka/usuarioejemplocrud/models/UsuarioModel.java
+++ src/main/java/com/sofka/usuarioejemplocrud/models/UsuarioModel.java
@@ -7,6 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/*Para mejorar: 
+1. Establecer validaciones por columna para denotar más coherencia de uso al momento de insertar datos
+2. Sobrecarga de constructores para definir prioridad por defecto al instanciar si se hacen los registros en el main
+3. Definición de prioridad por defecto
+*/
+
 @Entity
 @Table(name="tblusuario")
 public class UsuarioModel {
@@ -15,13 +21,17 @@ public class UsuarioModel {
     @Column(unique=true,nullable=false)
     private Long id;
 
-    //Para mejorar: Establecer validaciones para denotar más coherencia de uso al momento de insertar datos
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(unique=true,nullable=false)
     private String email;
+
+    @Column(nullable=false)
     private Integer prioridad;
 
 
-    
+    //Getters ans setters    
     public Long getId() {
         return id;
     }
