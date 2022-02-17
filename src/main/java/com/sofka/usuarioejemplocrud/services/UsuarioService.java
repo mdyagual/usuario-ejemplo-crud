c21708d84db55445fb921b1c74ade35941b51e19
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-17 00:14:17 -0500

    Implementación del método para buscar usuarios por una inicial y para devolver los usuarios ordenados por el atributo nombre

--- src/main/java/com/sofka/usuarioejemplocrud/services/UsuarioService.java
+++ src/main/java/com/sofka/usuarioejemplocrud/services/UsuarioService.java
@@ -1,15 +1,21 @@
 package com.sofka.usuarioejemplocrud.services;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.sofka.usuarioejemplocrud.models.UsuarioModel;
 import com.sofka.usuarioejemplocrud.repositories.UsuarioRepository;
-
+import com.sofka.usuarioejemplocrud.utils.UsuarioModelComparator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //Regla personal: En el servicio los métodos van en Inglés
+/*Para mejorar
+1. Creación de método para buscar usuarios por una inicial
+*/
 @Service
 public class UsuarioService {
     //Injección para usar aquí los métodos de la interfaz a la que se instancia
@@ -20,6 +26,14 @@ public class UsuarioService {
     public ArrayList<UsuarioModel> getUsuarios(){
         return (ArrayList<UsuarioModel>) uRepository.findAll();
     }
+    //Obtener todos los usuarios ordenados por el nombre
+    public ArrayList<UsuarioModel> getUsuariosNombre(){
+        //return (ArrayList<UsuarioModel>) uRepository.findAll();
+        ArrayList<UsuarioModel> uSinOrden = getUsuarios();
+        Collections.sort(uSinOrden,new UsuarioModelComparator());
+        return uSinOrden;
+    }
+
 
     //Guardar usuarios
     public UsuarioModel saveUsuario(UsuarioModel u){
@@ -37,8 +51,12 @@ public class UsuarioService {
     }
 
     //Obtener usuario(s) por inicial
-    public ArrayList<UsuarioModel> getByInitial(char c){
-        return uRepository.findByInicialNombre(c);
+    public List<UsuarioModel> getByInitial(char c){        
+        List<UsuarioModel> usuarios = getUsuarios();
+        List<UsuarioModel> uInicial= usuarios.stream()
+                                             .filter(l -> l.getNombre().charAt(0) == c)
+                                             .collect(Collectors.toList());
+        return uInicial;
     }
 
     //Eliminar usuario por id
