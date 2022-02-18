f87811071e185fe30ec714f100c835599d854ac4
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-17 21:10:17 -0500

    Eliminación CorreoInvalidoException porque era incorrecto el tratado

--- src/main/java/com/sofka/usuarioejemplocrud/exceptions/CorreoInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package com.sofka.usuarioejemplocrud.exceptions;
-
-public class CorreoInvalidoException extends Exception{
-
-    public CorreoInvalidoException(String message) {
-        super("Correo inválido, recuerde el formato: nombre_(numero)@mail.com");
-    }
-    
-}
